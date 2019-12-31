4e27dd3ce55195d75714dbca05a0f54477824bee
Jharell Chambers <jharell.chambers@selu.edu>
2019-12-31 19:05:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,15 +16,14 @@ class ApplicationController < Sinatra::Base
 		erb :signup
 	end
 
-	if params[:username] == "" || params[:password] == ""
-      redirect '/failure'
-    else
-      User.create(username: params[:username], password: params[:password])
-      redirect '/login'
-    end
-
-  end
-
+	post "/signup" do
+		user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
+	end
 
 	get "/login" do
 		erb :login
